c2253d16478021a580c0655debb5cc118f556c17
ASUS <minhthoai190892@gmail.com>
2023-09-06 10:56:40 +0700

    29.-Code-New-User-Function

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/controller/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/controller/UserController.java
@@ -6,18 +6,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.shopme.admin.user.UserRepository;
 import com.shopme.admin.user.service.UserService;
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
 @Controller
 public class UserController {
 	@Autowired 
 	private UserService userService;
+	
 	@GetMapping("/users")
 	public String listAll(Model model) {
 		List<User>listUsers = userService.listAll();
 		model.addAttribute("listUsers",listUsers);
 		return "users";
+	}
+	@GetMapping("/users/new")
+	public String newUser(Model model) {
+		User user = new User();
+		user.setEnabled(true);
+		List<Role> listRoles = userService.listRoles();
+
+		model.addAttribute("user",user);
+		model.addAttribute("listRoles",listRoles);
+		return "user_form";
+	}
+	@PostMapping("/users/save")
+	public String saveUser(User user,RedirectAttributes redirectAttributes) {
+		userService.saveUser(user);
+		redirectAttributes.addFlashAttribute("message","The user has been saved successfully.");
+		System.out.println(user);
+		System.out.println(user.getRoles());
+		return "redirect:/users";
 	}
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/service/UserService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/service/UserService.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.shopme.admin.user.RoleRepository;
 import com.shopme.admin.user.UserRepository;
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
 import jakarta.transaction.Transactional;
@@ -15,8 +17,16 @@ import jakarta.transaction.Transactional;
 public class UserService {
 	@Autowired 
 	private UserRepository userRepository;
+	@Autowired
+	private RoleRepository roleRepository;
 	public List<User> listAll() {
 		return userRepository.findAll();
 		
+	}
+	public List<Role> listRoles() {
+		return roleRepository.findAll();
+	}
+	public User saveUser(User user) {
+		return userRepository.save(user);
 	}
 }
