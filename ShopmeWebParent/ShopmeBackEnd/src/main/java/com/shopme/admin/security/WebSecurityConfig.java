9dfbc9fcdaa2f2f9db47169a4ca4463a07aebf31
ASUS <minhthoai190892@gmail.com>
2023-09-08 15:41:56 +0700

    51.-Code-User-Authentication

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.shopme.admin.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,10 +14,26 @@ public class WebSecurityConfig {
 	public PasswordEncoder   passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+
+
+	@Bean
+	 UserDetailsService userDetailsService() {
+		return new ShopmeUserDetailsService();
+	}
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.
-					authorizeHttpRequests(config->config.anyRequest().permitAll());
+					authorizeHttpRequests(config->config
+							.requestMatchers("/assest/**").permitAll()
+							.anyRequest().authenticated()
+							
+							)
+					.formLogin(login->login
+							.loginPage("/login")
+							.usernameParameter("email")
+							.permitAll()
+							)
+					;
 							
 
 		return httpSecurity.build();
